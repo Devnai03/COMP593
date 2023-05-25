b7517b4f5463176c0b38ac597e190db4312ae183
Devnai03 <dnai@flemingcollege.ca>
2023-05-24 23:03:13 -0400

    Added comment to script1.py

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
-print('Simple is better than complex.Complex is better than complicated.Flat is better than nested.')
-print('Although never is often better than *right* now.If the implementation is hard to explain, its a bad idea.')
+
+print('Flat is better than nested.')
+print('Sparse is better than dense.')
